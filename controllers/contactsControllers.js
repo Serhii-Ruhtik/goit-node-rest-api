dd9a69edb40cfce712ffddf76b7372f3c46f0e7b
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2024-01-22 23:14:10 +0200

    22.01

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,71 @@
-import contactsService from "../services/contactsServices.js";
+import {
+  listContactsService,
+  getContactByIdService,
+  removeContactService,
+  addContactService,
+  updateContactService,
+} from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await listContactsService();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await getContactByIdService(id);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    return res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await removeContactService(id);
+    if (!result) {
+      throw HttpError(404, "Not Found");
+    }
+    return res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const result = await addContactService(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  try {
+    const keys = Object.keys(req.body);
+
+    if (keys.length === 0) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+
+    const { id } = req.params;
+    const result = await updateContactService(id, req.body);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,5 +1,22 @@
 import Joi from "joi";
 
-export const createContactSchema = Joi.object({});
+export const createContactSchema = Joi.object({
+  name: Joi.string()
+    .required()
+    .messages({ "any.required": "Missing required name field" }),
 
-export const updateContactSchema = Joi.object({});
+  email: Joi.string()
+    .required()
+    .messages({ "any.required": "Missing required email field" }),
+  phone: Joi.string()
+    .required()
+    .messages({ "any.required": "Missing required phone field" }),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string(),
+
+  email: Joi.string(),
+
+  phone: Joi.string(),
+});
