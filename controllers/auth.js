5368722a77e52d1f4262f89f60b79df4bb4a3487
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2024-02-21 22:57:55 +0200

    21,02,24

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,8 +8,10 @@ import gravatar from "gravatar";
 import path from "path";
 import fs from "fs/promises";
 import Jimp from "jimp";
+import sendEmail from "../helpers/sendEmail.js";
+import { nanoid } from "nanoid";
 
-const { SECRET_KEY } = process.env;
+const { SECRET_KEY, BASE_URL } = process.env;
 const avatarsDir = path.resolve("public", "avatars");
 
 export const register = async (req, res, next) => {
@@ -22,12 +24,22 @@ export const register = async (req, res, next) => {
     }
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
+    const verificationToken = nanoid();
 
     const newUser = await User.create({
       ...req.body,
       password: hashPassword,
       avatarURL,
+      verificationToken,
     });
+
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Click here to verify email</a>`,
+    };
+
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
       user: {
---
+++ helpers/sendEmail.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+const { META_PASSWORD, EMAIL } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.meta.ua",
+  port: 465,
+  secure: true,
+  auth: {
+    user: EMAIL,
+    pass: META_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
+
+const sendEmail = async (data) => {
+  const email = { ...data, from: EMAIL };
+  await transport.sendMail(email);
+  return true;
+};
+export default sendEmail;
